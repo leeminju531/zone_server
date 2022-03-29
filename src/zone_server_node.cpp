272575935378c4d173b747368dcc92c7270408d0
leeminju531 <dlalswn531@naver.com>
2022-03-29 23:56:32 +0900

    Add : multi map publisher Only Latched Topic

--- src/zone_server_node.cpp
+++ src/zone_server_node.cpp
@@ -64,10 +64,9 @@ public:
       ROS_WARN("Zone Map Server Only Support Loading From Yaml Format");
       exit(-1);
     }
-
   }
+  
 
-private:
   MapMode mode = TRINARY;
   double res;
   int negate;
@@ -76,6 +75,7 @@ private:
   double origin[3];
   std::vector<std::string> zone_topic_name_;
   std::vector<std::string> zone_img_path_;
+  int img_cnt_;
   
   bool loadMapFromYaml(std::string path_to_yaml){
     std::ifstream fin(path_to_yaml.c_str());
@@ -157,10 +157,10 @@ private:
         mapfname = mapfpath.string();
       }
       for(auto const& dir_entry : boost::filesystem::directory_iterator{mapfpath}){
-        std::cout << dir_entry << std::endl;
+        //std::cout << dir_entry << std::endl;
         zone_img_path_.emplace_back(dir_entry.path().string());
       }
-
+      img_cnt_ = zone_img_path_.size();
       // extract topic name 
       for(const std::string& img_path : zone_img_path_){
         size_t s,e;
@@ -168,29 +168,95 @@ private:
         for(int i=img_path.size()-1;i>=0;i--){
           if(img_path[i] == '.')  e = i;
           else if(img_path[i] == '/')  s = i;
-
           if(s>0 && e>0)  break;
         }
         zone_topic_name_.emplace_back(img_path.substr(s+1,e-s-1));
       }
-      // for(int i=0;i<zone_topic_name_.size();i++){
-      //   std::cout << "topic name : "<<zone_topic_name_[i] << std::endl;
-      //   std::cout << "img path : "<<zone_img_path_[i] << std::endl;
-      // }
-
     }catch (YAML::InvalidScalar &) {
       ROS_ERROR("The map does not contain an image tag or it is invalid.");
       return false;
     }
-
-     
     return true;
   }
-
-
-  
 };
 
+class ZoneMapServer{
+
+public:
+  ZoneMapServer(MapInfo& mInfo) : zmInfo_(mInfo)
+  {
+    ros::NodeHandle private_nh("~");
+    private_nh.param("frame_id", frame_id_, std::string("map"));
+    for(int i=0;i<zmInfo_.img_cnt_;i++){
+      v_map_pub_.emplace_back(nh_.advertise<nav_msgs::OccupancyGrid>(zmInfo_.zone_topic_name_[i], 1, true));
+      v_metadata_pub_.emplace_back(nh_.advertise<nav_msgs::MapMetaData>( zmInfo_.zone_topic_name_[i] + "_metadata", 1, true));
+      //When called this service returns a copy of the current map
+      //get_map_service_ .emplace_back(nh_.advertiseService("static_" + zmInfo_.zone_topic_name_[i], &MapServer::mapCallback, this));
+      if(!loadMapFromValues(zmInfo_.zone_img_path_[i],zmInfo_.res,zmInfo_.negate,zmInfo_.occ_th,zmInfo_.free_th,zmInfo_.origin,zmInfo_.mode,i))
+      {
+        exit(-1);
+      }
+    }
+  }
+private:
+  MapInfo zmInfo_;
+  ros::NodeHandle nh_;
+  std::vector<ros::Publisher> v_map_pub_;
+  std::vector<ros::Publisher> v_metadata_pub_;
+  std::vector<ros::ServiceServer> get_map_service_;
+  std::vector<ros::ServiceServer> change_map_srv_;
+  std::string frame_id_;
+  
+  /** The map data is cached here, to be sent out to service callers
+     */
+  nav_msgs::MapMetaData meta_data_message_;
+  nav_msgs::GetMap::Response map_resp_;
+
+  /** Callback invoked when someone requests our service */
+  // bool mapCallback(nav_msgs::GetMap::Request  &req,
+  //                   nav_msgs::GetMap::Response &res )
+  // {
+  //   // request is empty; we ignore it
+
+  //   // = operator is overloaded to make deep copy (tricky!)
+  //   res = map_resp_;
+  //   ROS_INFO("Sending map");
+
+  //   return true;
+  // }
+
+  bool loadMapFromValues(std::string map_file_name, double resolution,
+                           int negate, double occ_th, double free_th,
+                           double origin[3], MapMode mode,int idx)
+  {
+    try {
+      map_server::loadMapFromFile(&map_resp_, map_file_name.c_str(),
+                                  resolution, negate, occ_th, free_th,
+                                  origin, mode);
+    } catch (std::runtime_error& e) {
+      ROS_WARN("Error While Loading map from image \"%s\"", map_file_name.c_str());
+      ROS_ERROR("%s", e.what());
+      return false;
+    }
+    // To make sure get a consistent time in simulation
+    ros::Time::waitForValid();
+    map_resp_.map.info.map_load_time = ros::Time::now();
+    map_resp_.map.header.frame_id = frame_id_;
+    map_resp_.map.header.stamp = ros::Time::now();
+    ROS_INFO("Read a %d X %d map @ %.3lf m/cell",
+              map_resp_.map.info.width,
+              map_resp_.map.info.height,
+              map_resp_.map.info.resolution);
+    meta_data_message_ = map_resp_.map.info;
+
+    //Publish latched topics
+    v_metadata_pub_[idx].publish( meta_data_message_ );
+    v_map_pub_[idx].publish( map_resp_.map );
+
+    
+    return true;
+  }
+};
 
 int main(int argc, char **argv)
 {
@@ -202,9 +268,8 @@ int main(int argc, char **argv)
   try
   {
     MapInfo mInfo(fname); // read yaml file info
-    
-
-
+    ZoneMapServer zms(mInfo);
+    ros::spin();
   }
   catch(std::runtime_error& e)
   {
