5a9cbfd9501e559c6c9334efb7f78aeffa243888
leeminju531 <dlalswn531@naver.com>
2022-04-04 19:38:52 +0900

    Update Code Structure

---
+++ src/main.cpp
@@ -0,0 +1,191 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include <unordered_map>
+
+#include "ros/ros.h"
+#include "ros/console.h"
+#include "map_server/image_loader.h"
+#include "nav_msgs/MapMetaData.h"
+#include "nav_msgs/LoadMap.h"
+#include "std_msgs/String.h"
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+#include <tf/tf.h>
+
+#include <string>
+using namespace std;
+
+#ifdef HAVE_YAMLCPP_GT_0_5_0
+// The >> operator disappeared in yaml-cpp 0.5, so this function is
+// added to provide support for code written under the yaml-cpp 0.3 API.
+template<typename T>
+void operator >> (const YAML::Node& node, T& i)
+{
+  i = node.as<T>();
+}
+#endif
+
+class RobotZoneServer{
+public:
+    RobotZoneServer():
+        private_nh_("~")
+    {
+        private_nh_.param("map_metadata", map_metadata_, std::string("map_metadata"));
+        private_nh_.param("global_frame_id", global_frame_id_, std::string("map"));
+        private_nh_.param("base_frame_id", base_frame_id_, std::string("base_footprint"));
+        private_nh_.param("zone_folder_path",zone_folder_path_,std::string(""));
+        
+        
+        robot_zone_pub_ = nh_.advertise<std_msgs::String> ("robot_zone",1);
+        map_metadata_sub_ = nh_.subscribe(map_metadata_,1,&RobotZoneServer::map_callback,this);
+        
+    }
+    
+private:
+    ros::NodeHandle private_nh_;
+    ros::NodeHandle nh_;
+    
+
+    string map_metadata_;
+    ros::Subscriber map_metadata_sub_;
+    double origin_[3];
+    double resolution_;
+    uint32_t width_;
+    uint32_t height_;
+
+    string global_frame_id_;
+    string base_frame_id_;
+    string zone_folder_path_;
+    
+    std::vector<std::string> zone_topic_name_;
+    std::vector<std::string> zone_img_path_;
+    std::vector<nav_msgs::MapMetaData> v_meta_data_message_;
+    std::vector<ros::Publisher> v_zone_meta_pub_;
+    std::vector<nav_msgs::GetMap::Response> v_zone_map_resp_;
+    std::vector<ros::Publisher> v_zone_map_pub_;
+    
+
+    ros::Publisher robot_zone_pub_;
+
+    void map_callback(const nav_msgs::MapMetaData::ConstPtr& msg){
+        origin_[0] = msg->origin.position.x;
+        origin_[1] = msg->origin.position.y;
+        tf::Quaternion q(
+          msg->origin.orientation.x,
+          msg->origin.orientation.y,
+          msg->origin.orientation.z,
+          msg->origin.orientation.w);
+        tf::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+        origin_[2] = yaw;
+
+        resolution_ = msg->resolution;
+        width_ = msg->width; // width , heigh 도 필요할까 ?
+        height_ = msg->height;
+        
+        ROS_INFO("Heard Map MetaData ! ");
+        
+        loadZoneMap(zone_folder_path_,origin_,resolution_);
+    }
+    void loadZoneMap(std::string z_path,double* origin,double resolution)
+    {
+      if(zone_folder_path_.empty())
+      {
+        ROS_WARN("The Zone Folder path param cannot be an empty string.");
+        exit(-1);
+      }
+      boost::filesystem::path mapfpath(zone_folder_path_);
+      if (!mapfpath.is_absolute()){
+        ROS_WARN("Specify Zone Folder Path as Absolute Path");
+        ROS_ERROR("Zone_server could not open %s.", zone_folder_path_.c_str());
+        exit(-1);
+      }
+      zone_img_path_.clear();
+      zone_topic_name_.clear();
+      v_zone_map_resp_.clear();
+      for(auto const& dir_entry : boost::filesystem::directory_iterator{mapfpath})
+      {
+        zone_img_path_.emplace_back(dir_entry.path().string());
+      }
+        
+      for(const std::string& img_path : zone_img_path_) // extract topic name by each pgm files. ex) '~~~/zoneFolder/{name}.pgm' -> {name}
+      {
+        size_t s,e;
+        s=e=0;
+        for(int i=img_path.size()-1;i>=0;i--)
+        {
+          if(img_path[i] == '.')  e = i;
+          else if(img_path[i] == '/')  s = i;
+          if(s>0 && e>0)  break;
+        }
+        zone_topic_name_.emplace_back(img_path.substr(s+1,e-s-1));
+      }
+      for(int i=0;i<zone_img_path_.size();i++)
+      {
+        ROS_INFO("Zone Pgm : %s",zone_img_path_[i].c_str());
+        ROS_INFO("assigned Topic Name : %s",zone_topic_name_[i].c_str());
+        v_zone_map_pub_.emplace_back(nh_.advertise<nav_msgs::OccupancyGrid>(zone_topic_name_[i],1,true));
+        v_zone_meta_pub_.emplace_back(nh_.advertise<nav_msgs::MapMetaData>(zone_topic_name_[i]+"_metadata",1,true));
+        if(!loadMapFromValues(zone_img_path_[i],resolution_,origin_))
+        {
+          exit(-1);
+        }
+      }
+
+    }
+    bool loadMapFromValues(std::string map_file_name,double resolution,double origin[3])
+    {
+      int negate = 0;
+      double occ_th = 0.65;
+      double free_th = 0.196;
+      MapMode mode = TRINARY;
+      nav_msgs::MapMetaData meta_data_message_;
+      nav_msgs::GetMap::Response map_resp_;
+      try 
+      {
+        map_server::loadMapFromFile(&map_resp_, map_file_name.c_str(),
+                                    resolution, negate, occ_th, free_th,
+                                    origin, mode);
+      } catch(std::runtime_error& e) 
+      {
+        ROS_WARN("Error While Loading map from image \"%s\"", map_file_name.c_str());
+        ROS_ERROR("%s", e.what());
+        return false;
+      }
+      ros::Time::waitForValid();
+      map_resp_.map.info.map_load_time = ros::Time::now();
+      map_resp_.map.header.frame_id = global_frame_id_;
+      map_resp_.map.header.stamp = ros::Time::now();
+      meta_data_message_ = map_resp_.map.info;
+      ROS_INFO("READ PGM : %s",map_file_name.c_str());
+      ROS_INFO("Read a %d X %d map @ %.3lf m/cell",
+              map_resp_.map.info.width,
+              map_resp_.map.info.height,
+              map_resp_.map.info.resolution);
+
+      //Publish latched topics
+      v_zone_map_pub_.back().publish(map_resp_.map);
+      v_zone_meta_pub_.back().publish(meta_data_message_);
+      v_meta_data_message_.emplace_back(meta_data_message_);
+      v_zone_map_resp_.emplace_back(map_resp_);
+
+
+      return true;
+    }
+
+
+};
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "robot_zone_sever", ros::init_options::AnonymousName);
+  
+  RobotZoneServer rzs;
+  ros::spin();
+
+  return 0;
+}
